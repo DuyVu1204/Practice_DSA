ac6999f1250d784896085cbf6fb7f08b58e3a705
DuyVu1204 <duyvu9906@gmail.com>
2025-04-21 15:36:52 +0700

    Update queue.cpp

--- 24120495_DSA_W05/queue.cpp
+++ 24120495_DSA_W05/queue.cpp
@@ -5,154 +5,185 @@
 
 using namespace std;
 
-struct NODE{
+struct NODE {
     int key;
     NODE* p_next;
 };
 
-struct Queue{
+struct Queue {
     NODE* head;
     NODE* tail;
 };
 
 Queue* initializeQueue();
-void enqueue(Queue &q, int key);
-int dequeue(Queue &q);
+void enqueue(Queue& q, int key);
+int dequeue(Queue& q);
 int size(Queue q);
 bool isEmpty(Queue q);
+void clearQueue(Queue& q);//Add queue free function to avoid duplication
 
-Queue* initializeQueue(){
-    Queue* q=new Queue;
-    q->head=nullptr;
-    q->tail=nullptr;
+Queue* initializeQueue() {
+    Queue* q = new Queue;
+    q->head = nullptr;
+    q->tail = nullptr;
     return q;
 }
 
-void enqueue(Queue &q, int key){
-    NODE* newNode=new NODE();
-    newNode->key=key;
-    newNode->p_next=nullptr;
-    if (q.head==nullptr)
+void enqueue(Queue& q, int key) {
+    NODE* newNode = new NODE();
+    newNode->key = key;
+    newNode->p_next = nullptr;
+    if (q.head == nullptr)
     {
-        q.head=newNode;
-        q.tail=newNode;
+        q.head = newNode;
+        q.tail = newNode;
     }
     else
     {
-        q.tail->p_next=newNode;
-        q.tail=newNode;
+        q.tail->p_next = newNode;
+        q.tail = newNode;
     }
 }
 
-int dequeue(Queue &q){
+int dequeue(Queue& q) {
+    if (q.head == nullptr)
+        return -1;
     int x;
-    if (q.head==nullptr)
-    return -1;
-        NODE* temp=q.head;
-        x=temp->key;
-        if (q.head==q.tail)
-        {
-            q.head=nullptr;
-            q.tail=nullptr;
-        }
-        else
-        {
-            q.head=q.head->p_next;
-        }
-        delete temp;
+    NODE* temp = q.head;
+    x = temp->key;
+    if (q.head == q.tail)
+    {
+        q.head = nullptr;
+        q.tail = nullptr;
+    }
+    else
+    {
+        q.head = q.head->p_next;
+    }
+    delete temp;
     return x;
 }
 
-int size(Queue q){
-    if (q.head==nullptr)
-    return 0;
-    NODE* temp=q.head;
-    int count=0;
-    while (temp!=nullptr)
+int size(Queue q) {
+    if (q.head == nullptr)
+        return 0;
+    NODE* temp = q.head;
+    int count = 0;
+    while (temp != nullptr)
     {
         count++;
-        temp=temp->p_next;
+        temp = temp->p_next;
     }
     return count;
 }
 
-bool isEmpty(Queue q){
-    if (q.head==nullptr)
-    return true;
+bool isEmpty(Queue q) {
+    if (q.head == nullptr)
+        return true;
     else
-    return false;
+        return false;
 }
 
-int main(){
-    Queue* q;
+void clearQueue(Queue& q) {
+     while (!isEmpty(q))
+     {
+        dequeue(q);
+     }
+}
+
+
+int main() {
+    Queue* q = nullptr;
+    q = initializeQueue();
     ifstream file("input.txt");
     if (!file)
-    cout<<"Error file"<<endl;
+    {
+        cout << "Error open file" << endl;
+        return 1;
+    }
     ofstream outfile("output.txt");
     if (!outfile)
-    cout<<"Error file"<<endl;
+    {
+        cout << "Error open file" << endl;
+        return 1;
+    }
     string line;
-    while (getline(file,line))
+    while (getline(file, line))
     {
         stringstream ss(line);
         string call;
-        ss>>call;
-        if (call=="init")
+        ss >> call;
+        if (call == "init")
         {
-            q=initializeQueue();
-            outfile<<"EMPTY"<<endl;
+            if (q != nullptr)
+            {
+                while (!isEmpty(*q))
+                {
+                    dequeue(*q);
+                }
+                delete q;
+            }
+            q = initializeQueue();
+
+            outfile << "EMPTY" << endl;
         }
-        else if(call=="enqueue")
+        else if (call == "enqueue")
         {
             int x;
-            ss>>x;
-            enqueue(*q,x);
-            NODE* temp=q->head;
-            while (temp!=nullptr)
+            ss >> x;
+            enqueue(*q, x);
+            NODE* temp = q->head;
+            while (temp != nullptr)
             {
-                outfile<<temp->key<<" ";
-                temp=temp->p_next;
+                outfile << temp->key << " ";
+                temp = temp->p_next;
             }
-            outfile<<endl;
+            outfile << endl;
         }
-        else if (call=="dequeue")
+        else if (call == "dequeue")
         {
-            if (q->head==nullptr)
+            if (isEmpty(*q))
             {
-                outfile<<"EMPTY"<<endl;
+                outfile << "EMPTY" << endl;
             }
             else
             {
                 dequeue(*q);
-                if (q->head==nullptr)
+                if (isEmpty(*q))
                 {
-                    outfile<<"EMPTY"<<endl;
+                    outfile << "EMPTY" << endl;
                 }
                 else
                 {
-                    NODE* temp=q->head;
-                    while (temp!=nullptr)
+                    NODE* temp = q->head;
+                    while (temp != nullptr)
                     {
-                        outfile<<temp->key<<" ";
-                        temp=temp->p_next;
+                        outfile << temp->key << " ";
+                        temp = temp->p_next;
                     }
-                    outfile<<endl;
+                    outfile << endl;
                 }
             }
         }
-        else if (call=="size")
+        else if (call == "size")
         {
-            outfile<<size(*q)<<endl;
+            outfile << size(*q) << endl;
         }
-        else if (call=="isEmpty")
+        else if (call == "isEmpty")
         {
-            if (isEmpty(*q))
-            outfile<<"TRUE"<<endl;
-            else 
-            outfile<<"FALSE"<<endl;
+
+            if (!isEmpty(*q))
+                outfile << "FALSE" << endl;
+            else
+                outfile << "TRUE" << endl;
         }
     }
     file.close();
     outfile.close();
+    if (q != nullptr)//free up memory
+    {
+        clearQueue(*q);
+        delete q;
+    }
     return 0;
 }
